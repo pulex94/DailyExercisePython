6214c0496d9b26e451aaddf105c3494887ea7be6
Alex Lazzarotto <146884321+pulex94@users.noreply.github.com>
2026-05-12 19:02:35 +0200

    Finished Day5 project

--- Day5_Inventory_Manager/script.py
+++ Day5_Inventory_Manager/script.py
@@ -15,18 +15,30 @@ with open("inventory.txt", "r") as f:
         inventory.append(prodotto)
     del inventory[0]
 # 3. Identify products below minimum stock (quantity < min_stock)
+    belowStock = []
     for element in inventory:
-        if int(element["quantity"]) > int(element["min_stock"]):
-            print(f"{element["product"]}")
+        if int(element["quantity"]) < int(element["min_stock"]):
+            belowStock.append(element["product"])
+            print(f"Below minimum stock: {element["product"]}")
 # 4. Calculate how many units need to be ordered (min_stock - quantity)
+    toOrder = int(element["min_stock"]) - int(element["quantity"])
+    for element in inventory:
+        if toOrder > 0 :
+            print(f"{element["product"]} - To order: {toOrder}")     
+# 5. Calculate the reorder cost for each product (units_to_order * price)
+    totalCost = 0
     for element in inventory:
         toOrder = int(element["min_stock"]) - int(element["quantity"])
-        if toOrder > 0 :
-            print(f"{element["product"]} - To order: {toOrder}")   
-# 4. Calculate how many units need to be ordered (min_stock - quantity)
-# 5. Calculate the reorder cost for each product (units_to_order * price)
+        reorderCost = int(toOrder) * float(element["price"])
+        if toOrder > 0:
+            print(f"Reorder cost for {element["product"]} is: {round(reorderCost)}")
 # 6. Calculate the total reorder cost
+            totalCost = totalCost + reorderCost
+    print(f"Total Reorder cost: {totalCost}")
 # 7. Print a clean reorder report
+    print(f"While looking at this dates we can see that:\nWe have some articles below minimum stock:")
+    [print(x) for x in belowStock]
+    print(f"The total cost for reorder everything is: {totalCost}")
 # The file has 4 fields separated by ": "
 # Use split(": ") to parse each line
 # Skip the first line (header)
